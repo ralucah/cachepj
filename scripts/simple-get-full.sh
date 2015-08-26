003480f4ea3512d03dce7a57c48a09ea3a12aa1a
ralucah <ralucahalalai@gmail.com>
2015-08-26 14:56:36 +0200

    simple get full data

--- scripts/simple-get-full.sh
+++ scripts/simple-get-full.sh
@@ -5,7 +5,6 @@ IP2="52.28.116.209"
 IP3="54.64.186.40"
 
 TESTS=5
-P=2
 K=6
 
 if [ $# -ne 1 ] ; then
@@ -18,26 +17,34 @@ FILE=$1
 
 
 RAMDISK="/media/ramdsk"
-mkdir -p $RAMDISK
-sudo mount -t tmpfs -o size=1024M tmpfs /media/ramdsk
-mount
+#mkdir -p $RAMDISK
+#sudo mount -t tmpfs -o size=1024M tmpfs $RAMDISK
 
 function get_full() {
     IP=$1
+    P=$2
+    rm $RAMDISK/* 2>/dev/null
     START=`date +%s%N | cut -b1-13`
-    aria2c --dir=$RAMDISK  -x $P -s $P http://$IP/$FULL
+    aria2c -q --dir=$RAMDISK  -x $P -s $P http://$IP/$FILE
     STOP=`date +%s%N | cut -b1-13`
     MS=$(( $STOP - $START ))
     SEC=`bc <<< "scale = 3; $MS / 1000"`
     printf "%0.3f" $SEC
 }
 
-echo "SITE1 SITE2 SITE3"
+echo "P2SITE1 P2SITE2 P2SITE3 P1SITE1 P1SITE2 P1SITE3"
 for (( T=1;T<=$TESTS;T++ )) ; do
-    get_full $IP1
+    get_full $IP1 2
     printf " "
-    get_full $IP2
+    get_full $IP2 2
     printf " "
-    get_full $IP3
+    get_full $IP3 2
+    printf " "
+
+    get_full $IP1 1
+    printf " "
+    get_full $IP2 1
+    printf " "
+    get_full $IP3 1
     printf "\n"
 done
