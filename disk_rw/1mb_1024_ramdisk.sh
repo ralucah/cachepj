92383b2efabd8cd86df5e01f251be48dbd195688
ralucah <ralucahalalai@gmail.com>
2015-09-04 18:58:45 +0200

    disk ops

---
+++ disk_rw/1mb_1024_ramdisk.sh
@@ -0,0 +1,20 @@
+---Read ---
+1024+0 records in
+1024+0 records out
+1073741824 bytes (1.1 GB) copied, 0.163675 s, 6.6 GB/s
+---Read ---
+1024+0 records in
+1024+0 records out
+1073741824 bytes (1.1 GB) copied, 0.148801 s, 7.2 GB/s
+---Read ---
+1024+0 records in
+1024+0 records out
+1073741824 bytes (1.1 GB) copied, 0.145553 s, 7.4 GB/s
+---Read ---
+1024+0 records in
+1024+0 records out
+1073741824 bytes (1.1 GB) copied, 0.145399 s, 7.4 GB/s
+---Read ---
+1024+0 records in
+1024+0 records out
+1073741824 bytes (1.1 GB) copied, 0.145403 s, 7.4 GB/s
