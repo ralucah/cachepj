1de244de255769eec1e204f8be71ec324d6f22c9
ralucah <ralucahalalai@gmail.com>
2015-09-07 18:37:08 +0200

    ramdisk

--- disk_rw/1mb_1024_ramdisk.sh
+++ disk_rw/1mb_1024_ramdisk.sh
@@ -1,20 +1,25 @@
+ubuntu@ip-172-31-13-174:~$ ./measure-disk.sh 
+--- Write ---
+1024+0 records in
+1024+0 records out
+1073741824 bytes (1.1 GB) copied, 98.5277 s, 10.9 MB/s
 ---Read ---
 1024+0 records in
 1024+0 records out
-1073741824 bytes (1.1 GB) copied, 0.163675 s, 6.6 GB/s
+1073741824 bytes (1.1 GB) copied, 0.174751 s, 6.1 GB/s
 ---Read ---
 1024+0 records in
 1024+0 records out
-1073741824 bytes (1.1 GB) copied, 0.148801 s, 7.2 GB/s
+1073741824 bytes (1.1 GB) copied, 0.158517 s, 6.8 GB/s
 ---Read ---
 1024+0 records in
 1024+0 records out
-1073741824 bytes (1.1 GB) copied, 0.145553 s, 7.4 GB/s
+1073741824 bytes (1.1 GB) copied, 0.15673 s, 6.9 GB/s
 ---Read ---
 1024+0 records in
 1024+0 records out
-1073741824 bytes (1.1 GB) copied, 0.145399 s, 7.4 GB/s
+1073741824 bytes (1.1 GB) copied, 0.156625 s, 6.9 GB/s
 ---Read ---
 1024+0 records in
 1024+0 records out
-1073741824 bytes (1.1 GB) copied, 0.145403 s, 7.4 GB/s
+1073741824 bytes (1.1 GB) copied, 0.1566 s, 6.9 GB/s
