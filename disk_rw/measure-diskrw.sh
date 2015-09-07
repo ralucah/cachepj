e989e3edbcd2c04895a0a06522d5cda78cec866d
ralucah <ralucahalalai@gmail.com>
2015-09-07 12:10:05 +0200

    script

--- disk_rw/measure-diskrw.sh
+++ disk_rw/measure-diskrw.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 FILENAME=/tmp/test
-BLOCKSIZE=50M
-BLOCKCOUNT=20
+BLOCKSIZE=1M
+BLOCKCOUNT=1024
 TESTS=5
 
 for (( I=1;I<=$TESTS;I++ )) ; do
@@ -14,8 +14,8 @@ for (( I=1;I<=$TESTS;I++ )) ; do
     sync && sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
 
     echo '---Read ---'
-    #dd if=$FILENAME of=/dev/null bs=$BLOCKSIZE count=$BLOCKCOUNT
-    scp $FILENAME localhost:/dev/null
+    dd if=$FILENAME of=/dev/null bs=$BLOCKSIZE count=$BLOCKCOUNT
+    #scp $FILENAME localhost:/dev/null
 
     #rm -f $FILENAME
 done
